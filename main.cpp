ddab1e36f56d2bc4b547368644c243bf1c2f31bc
Tongeang-Thet <thettongeang@gmail.com>
2024-09-03 11:20:21 +0700

    update: updated on main file

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int main(){
     system("cls");
     cout << "Hello Github!" << endl;
+    string username;
+    cout << "Enter your username: " << username << endl;
+    cin >> username;
+    cout << "Hello, " << username << "Welcome to Github!" << endl;
 
     return 0;
 }
